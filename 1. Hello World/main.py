8bee3be1b667fe33fe7d90b990aed26870bb682f
bbbpppolly <bernard@weeklyhow.com>
2023-04-13 00:28:15 +0200

    Hello to Python!

---
+++ 1. Hello World/main.py
@@ -0,0 +1,5 @@
+print("Hello World!");
+
+print("Oops, noticed I used semi-colon? You don't have to use that in Python!")
+
+print("The print function is used to output something in the console")
---
+++ 2. Comments and Pound Character/Main.py
@@ -0,0 +1,9 @@
+print("To comment in Python, we can't use // or /***/.")
+
+print("Instead, we use pound character or number sign symbol (#)")
+
+# This is an example comment
+
+# This is useful for documenting your code
+# However, too much comment is unnecessary
+# Keep it short and simple (KISS)
